b6179b4b4b71b863b55d37f22902c7a2c6cc149f
Дмитрий Хорошко <datfeelman@gmail.com>
2022-07-16 15:37:10 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        int[] numbers = new int[]{'1', '2', '0'}; // Неправильно
+        int[] numbers1 = {1, 2, 3}; // Вот так
+        System.out.println('1');
+    }
+
+}
+
+
